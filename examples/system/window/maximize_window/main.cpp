42b5fb5056ae8dbb8ae713523d5acea0ea1b1f70
kavos113 <kohei.hadano@gmail.com>
2025-06-03 23:10:54 +0900

    add ci

---
+++ examples/system/window/maximize_window/main.cpp
@@ -0,0 +1,13 @@
+#include <karin/system.h>
+
+int main()
+{
+    karin::Application& app = karin::Application::instance();
+    karin::Window window(L"Hello Window", 100, 100, 800, 600);
+
+    window.setStatus(karin::Window::ShowStatus::MAXIMIZE);
+
+    app.run();
+
+    return 0;
+}
