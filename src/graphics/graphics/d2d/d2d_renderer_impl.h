0477fb1a07882eb541a9ad6cf59985b6199e70fe
kavos113 <kohei.hadano@gmail.com>
2025-06-14 16:39:27 +0900

    fix

--- src/graphics/graphics/d2d/d2d_renderer_impl.h
+++ src/graphics/graphics/d2d/d2d_renderer_impl.h
@@ -38,7 +38,7 @@ private:
 
     Microsoft::WRL::ComPtr<ID2D1DeviceContext> m_deviceContext;
 
-    const D2D1_COLOR_F m_clearColor = D2D1::ColorF(D2D1::ColorF::White);
+    D2D1_COLOR_F m_clearColor = D2D1::ColorF(D2D1::ColorF::White);
 
     static constexpr float DEFAULT_DPI = 96.0f; // Default DPI for D2D
     const D2D1_BITMAP_PROPERTIES1 bitmapProperties = D2D1::BitmapProperties1(
