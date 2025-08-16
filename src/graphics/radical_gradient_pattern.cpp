a6f1b92cf8b71115de78c9c7827dd4b6dd8b3dd9
kavos113 <kohei.hadano@gmail.com>
2025-08-16 11:22:46 +0900

    feat: radical gradient hash

---
+++ src/graphics/radical_gradient_pattern.cpp
@@ -0,0 +1,17 @@
+#include <karin/graphics/radical_gradient_pattern.h>
+#include <utils/hash.h>
+
+size_t karin::RadicalGradientPattern::hash() const
+{
+    size_t seed = 0;
+
+    hash_combine(seed, center.x);
+    hash_combine(seed, center.y);
+    hash_combine(seed, offset.x);
+    hash_combine(seed, offset.y);
+    hash_combine(seed, radiusX);
+    hash_combine(seed, radiusY);
+    hash_combine(seed, gradientPoints.hash());
+
+    return seed;
+}
