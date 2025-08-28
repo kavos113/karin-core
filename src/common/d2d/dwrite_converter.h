794cd5290de55d13037fbeff5c55ec367c2c3833
kavos113 <kohei.hadano@gmail.com>
2025-08-29 08:07:19 +0900

    feat: converter from dwrite to karin

--- src/common/d2d/dwrite_converter.h
+++ src/common/d2d/dwrite_converter.h
@@ -80,6 +80,79 @@ inline DWRITE_FONT_STRETCH toDWriteFontStretch(Font::Stretch stretch)
         throw std::invalid_argument("Unknown font stretch");
     }
 }
+
+inline Font::Weight fromDWriteFontWeight(DWRITE_FONT_WEIGHT weight)
+{
+    switch (weight)
+    {
+    case DWRITE_FONT_WEIGHT_THIN:
+        return Font::Weight::THIN;
+    case DWRITE_FONT_WEIGHT_EXTRA_LIGHT:
+        return Font::Weight::EXTRA_LIGHT;
+    case DWRITE_FONT_WEIGHT_LIGHT:
+        return Font::Weight::LIGHT;
+    case DWRITE_FONT_WEIGHT_SEMI_LIGHT:
+        return Font::Weight::SEMI_LIGHT;
+    case DWRITE_FONT_WEIGHT_NORMAL:
+        return Font::Weight::NORMAL;
+    case DWRITE_FONT_WEIGHT_MEDIUM:
+        return Font::Weight::MEDIUM;
+    case DWRITE_FONT_WEIGHT_SEMI_BOLD:
+        return Font::Weight::SEMI_BOLD;
+    case DWRITE_FONT_WEIGHT_BOLD:
+        return Font::Weight::BOLD;
+    case DWRITE_FONT_WEIGHT_EXTRA_BOLD:
+        return Font::Weight::EXTRA_BOLD;
+    case DWRITE_FONT_WEIGHT_BLACK:
+        return Font::Weight::BLACK;
+    case DWRITE_FONT_WEIGHT_EXTRA_BLACK:
+        return Font::Weight::EXTRA_BLACK;
+    default:
+        throw std::invalid_argument("Unknown DWRITE font weight");
+    }
+}
+
+inline Font::Style fromDWriteFontStyle(DWRITE_FONT_STYLE style)
+{
+    switch (style)
+    {
+    case DWRITE_FONT_STYLE_NORMAL:
+        return Font::Style::NORMAL;
+    case DWRITE_FONT_STYLE_ITALIC:
+        return Font::Style::ITALIC;
+    case DWRITE_FONT_STYLE_OBLIQUE:
+        return Font::Style::OBLIQUE;
+    default:
+        throw std::invalid_argument("Unknown DWRITE font style");
+    }
+}
+
+inline Font::Stretch fromDWriteFontStretch(DWRITE_FONT_STRETCH stretch)
+{
+    switch (stretch)
+    {
+    case DWRITE_FONT_STRETCH_ULTRA_CONDENSED:
+        return Font::Stretch::ULTRA_CONDENSED;
+    case DWRITE_FONT_STRETCH_EXTRA_CONDENSED:
+        return Font::Stretch::EXTRA_CONDENSED;
+    case DWRITE_FONT_STRETCH_CONDENSED:
+        return Font::Stretch::CONDENSED;
+    case DWRITE_FONT_STRETCH_SEMI_CONDENSED:
+        return Font::Stretch::SEMI_CONDENSED;
+    case DWRITE_FONT_STRETCH_NORMAL:
+        return Font::Stretch::NORMAL;
+    case DWRITE_FONT_STRETCH_SEMI_EXPANDED:
+        return Font::Stretch::SEMI_EXPANDED;
+    case DWRITE_FONT_STRETCH_EXPANDED:
+        return Font::Stretch::EXPANDED;
+    case DWRITE_FONT_STRETCH_EXTRA_EXPANDED:
+        return Font::Stretch::EXTRA_EXPANDED;
+    case DWRITE_FONT_STRETCH_ULTRA_EXPANDED:
+        return Font::Stretch::ULTRA_EXPANDED;
+    default:
+        throw std::invalid_argument("Unknown DWRITE font stretch");
+    }
+}
 }
 
 #endif //SRC_COMMON_D2D_DWRITE_CONVERTER_H
