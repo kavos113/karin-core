4effcca943e39da069f0d1179472ee24c730a739
kavos113 <kohei.hadano@gmail.com>
2025-08-29 20:22:38 +0900

    feat: load font

--- src/graphics/text/font_loader.cpp
+++ src/graphics/text/font_loader.cpp
@@ -4,7 +4,8 @@
 
 namespace karin
 {
-FontLoader::FontLoader()
+FontLoader::FontLoader(SystemFont* systemFont)
+    : m_systemFont(systemFont)
 {
     FT_Error error = FT_Init_FreeType(&m_library);
     if (error)
@@ -16,5 +17,35 @@ FontLoader::FontLoader()
 FontLoader::~FontLoader()
 {
     FT_Done_FreeType(m_library);
+}
+
+FT_Face FontLoader::loadFont(const Font& font)
+{
+    if (auto it = m_faces.find(font.hash()); it != m_faces.end())
+    {
+        return it->second;
+    }
+
+    FT_Face face = nullptr;
+    auto fontData = m_systemFont->getFontData(font);
+    if (fontData.empty())
+    {
+        throw std::runtime_error("Font not found: " + font.family);
+    }
+
+    FT_Error error = FT_New_Memory_Face(
+        m_library,
+        reinterpret_cast<const FT_Byte*>(fontData.data()),
+        static_cast<FT_Long>(fontData.size()),
+        0,
+        &face
+    );
+    if (error)
+    {
+        throw std::runtime_error("Failed to create font face from memory");
+    }
+
+    m_faces[font.hash()] = face;
+    return face;
 }
 } // karin
--- src/graphics/text/font_loader.h
+++ src/graphics/text/font_loader.h
@@ -1,10 +1,11 @@
 #ifndef SRC_GRAPHICS_TEXT_FONT_LOADER_H
 #define SRC_GRAPHICS_TEXT_FONT_LOADER_H
 
+#include <karin/system/system_font.h>
+
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
-#include <string>
 #include <unordered_map>
 
 namespace karin
@@ -12,13 +13,17 @@ namespace karin
 class FontLoader
 {
 public:
-    FontLoader();
+    FontLoader(SystemFont* systemFont);
     ~FontLoader();
 
+    FT_Face loadFont(const Font& font);
+
 private:
+    SystemFont* m_systemFont = nullptr;
+
     FT_Library m_library = nullptr;
 
-    std::unordered_map<std::string, FT_Face> m_faces;
+    std::unordered_map<size_t, FT_Face> m_faces;
 };
 } // karin
 
