bb3fbae165a4701d63ffa5ba9f73ef28bccb593a
kavos113 <kohei.hadano@gmail.com>
2025-10-23 21:33:40 +0900

    feat: transform has scale/rot/translation parameter, not matrix

--- include/karin/common/geometry/transform2d.h
+++ include/karin/common/geometry/transform2d.h
@@ -1,7 +1,10 @@
 #ifndef KARIN_COMMON_GEOMETRY_TRANSFORM2D_H
 #define KARIN_COMMON_GEOMETRY_TRANSFORM2D_H
 
-#include <memory>
+#include <glm/glm.hpp>
+#include <glm/ext/matrix_transform.hpp>
+
+#include "point.h"
 
 namespace karin
 {
@@ -10,24 +13,78 @@ class Transform2DImpl;
 class Transform2D
 {
 public:
-    Transform2D();
-    ~Transform2D();
+    Transform2D() = default;
+    ~Transform2D() = default;
 
-    Transform2D& translate(float tx, float ty);
+    Transform2D& translate(float tx, float ty)
+    {
+        m_translation.x += tx;
+        m_translation.y += ty;
+        return *this;
+    }
+    Transform2D& setTranslation(float tx, float ty)
+    {
+        m_translation.x = tx;
+        m_translation.y = ty;
+        return *this;
+    }
 
     // radians. counter-clockwise
-    Transform2D& rotate(float radian);
-    Transform2D& rotateDeg(float degree);
+    Transform2D& rotate(float radian)
+    {
+        m_rotation += radian;
+        return *this;
+    }
+    Transform2D& rotateDeg(float degree)
+    {
+        return rotate(glm::radians(degree));
+    }
+    Transform2D& setRotation(float radian)
+    {
+        m_rotation = radian;
+        return *this;
+    }
+    Transform2D& setRotationDeg(float degree)
+    {
+        return setRotation(glm::radians(degree));
+    }
 
-    Transform2D& scale(float sx, float sy);
+    Transform2D& scale(float sx, float sy)
+    {
+        m_scale.x *= sx;
+        m_scale.y *= sy;
+        return *this;
+    }
+    Transform2D& setScale(float sx, float sy)
+    {
+        m_scale.x = sx;
+        m_scale.y = sy;
+        return *this;
+    }
 
-    const float* data() const;
+    glm::mat4 toMatrix() const
+    {
+        return glm::scale(glm::mat4(1.0f), glm::vec3(m_scale.x, m_scale.y, 1.0f))
+            * glm::rotate(glm::mat4(1.0f), m_rotation, glm::vec3(0.0f, 0.0f, 1.0f))
+            * glm::translate(glm::mat4(1.0f), glm::vec3(m_translation.x, m_translation.y, 0.0f));
+    }
 
 private:
-    std::unique_ptr<Transform2DImpl> m_impl;
+    Point m_translation{0.0f, 0.0f};
+    float m_rotation = 0.0f; // radians
+    Point m_scale{1.0f, 1.0f};
 };
 
-std::ostream& operator<<(std::ostream& os, const Transform2D& transform);
+std::ostream& operator<<(std::ostream& os, const Transform2D& transform)
+{
+    glm::mat4 mat = transform.toMatrix();
+    const float* d = &mat[0][0];
+    return os << "Transform Matrix:\n"
+       << d[0] << " " << d[4] << " " << d[8]  << " " << d[12] << "\n"
+       << d[1] << " " << d[5] << " " << d[9]  << " " << d[13] << "\n"
+       << d[2] << " " << d[6] << " " << d[10] << " " << d[14] << "\n"
+       << d[3] << " " << d[7] << " " << d[11] << " " << d[15] << "\n";
+}
 } // karin
 
 #endif //KARIN_COMMON_GEOMETRY_TRANSFORM2D_H
