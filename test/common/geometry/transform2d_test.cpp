bc9a478c875181db1b6b7307da1eacce4e0d7067
kavos113 <kohei.hadano@gmail.com>
2025-10-23 21:46:50 +0900

    Revert "fix: transform 2d test"
    This reverts commit e7d42d37328e815ed84d20a39e75aa3c07a32234.

--- test/common/geometry/transform2d_test.cpp
+++ test/common/geometry/transform2d_test.cpp
@@ -17,8 +17,7 @@ protected:
 
 TEST_F(Transform2DTest, InitialMatrixIsIdentity) {
     Transform2D t;
-    glm::mat4 mat = t.toMatrix();
-    const float* d = &mat[0][0];
+    const float* d = t.data();
     glm::mat4 expected(1.0f);
     for (int i = 0; i < 16; ++i) {
         EXPECT_FLOAT_EQ(d[i], (&expected[0][0])[i]);
@@ -28,8 +27,7 @@ TEST_F(Transform2DTest, InitialMatrixIsIdentity) {
 TEST_F(Transform2DTest, Translate) {
     Transform2D t;
     t.translate(1.0f, 2.0f);
-    glm::mat4 mat = t.toMatrix();
-    const float* d = &mat[0][0];
+    const float* d = t.data();
     glm::mat4 expected = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 2.0f, 0.0f));
     for (int i = 0; i < 16; ++i) {
         EXPECT_FLOAT_EQ(d[i], (&expected[0][0])[i]);
@@ -40,8 +38,7 @@ TEST_F(Transform2DTest, Rotate) {
     Transform2D t;
     float angle = glm::radians(45.0f);
     t.rotate(angle);
-    glm::mat4 mat = t.toMatrix();
-    const float* d = &mat[0][0];
+    const float* d = t.data();
     glm::mat4 expected = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.0f, 0.0f, 1.0f));
     for (int i = 0; i < 16; ++i) {
         EXPECT_FLOAT_EQ(d[i], (&expected[0][0])[i]);
@@ -51,8 +48,7 @@ TEST_F(Transform2DTest, Rotate) {
 TEST_F(Transform2DTest, Scale) {
     Transform2D t;
     t.scale(2.0f, 3.0f);
-    glm::mat4 mat = t.toMatrix();
-    const float* d = &mat[0][0];
+    const float* d = t.data();
     glm::mat4 expected = glm::scale(glm::mat4(1.0f), glm::vec3(2.0f, 3.0f, 1.0f));
     for (int i = 0; i < 16; ++i) {
         EXPECT_FLOAT_EQ(d[i], (&expected[0][0])[i]);
@@ -62,8 +58,7 @@ TEST_F(Transform2DTest, Scale) {
 TEST_F(Transform2DTest, CombinedTransformations) {
     Transform2D t;
     t.translate(1.0f, 2.0f).rotate(glm::radians(90.0f)).scale(2.0f, 2.0f);
-    glm::mat4 mat = t.toMatrix();
-    const float* d = &mat[0][0];
+    const float* d = t.data();
     glm::mat4 expected = glm::translate(
         glm::scale(
             glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f)),
@@ -79,18 +74,15 @@ TEST_F(Transform2DTest, CombinedTransformations) {
 TEST_F(Transform2DTest, CombinedTransformationsIsNoParticularOrder) {
     Transform2D t1;
     t1.translate(1.0f, 2.0f).rotate(glm::radians(90.0f)).scale(2.0f, 2.0f);
-    glm::mat4 mat = t1.toMatrix();
-    const float* d1 = &mat[0][0];
+    const float* d1 = t1.data();
 
     Transform2D t2;
     t2.scale(2.0f, 2.0f).rotate(glm::radians(90.0f)).translate(1.0f, 2.0f);
-    glm::mat4 mat2 = t2.toMatrix();
-    const float* d2 = &mat[0][0];
+    const float* d2 = t2.data();
 
     Transform2D t3;
     t3.rotate(glm::radians(90.0f)).scale(2.0f, 2.0f).translate(1.0f, 2.0f);
-    glm::mat4 mat3 = t3.toMatrix();
-    const float* d3 = &mat[0][0];
+    const float* d3 = t3.data();
 
     for (int i = 0; i < 16; ++i) {
         EXPECT_FLOAT_EQ(d1[i], d2[i]);
