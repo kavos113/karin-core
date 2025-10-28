acd2c1c67fe9bf278a08616ac5c189724750b4a7
kavos113 <kohei.hadano@gmail.com>
2025-10-28 13:43:11 +0900

    feat: hb init in font layouter

--- src/graphics/text/font_layouter.cpp
+++ src/graphics/text/font_layouter.cpp
@@ -40,44 +40,58 @@ std::vector<FontLayouter::GlyphPosition> FontLayouter::layout(const TextLayout &
         throw std::runtime_error("failed to set pixel sizes for font");
     }
 
-    std::vector<std::string> lines = std::views::split(layout.text, '\n')
-        | std::views::transform(
-              [](auto &&rng)
-              {
-                  return std::string(rng.begin(), rng.end());
-              })
-        | std::ranges::to<std::vector<std::string>>();
+    hb_font_t* hbFont = hb_ft_font_create(face, nullptr);
+    hb_buffer_t* hbBuffer = hb_buffer_create();
+
+    hb_buffer_add_utf8(hbBuffer, layout.text.c_str(), -1, 0, -1);
+    hb_buffer_set_direction(hbBuffer, toHBDirection(layout.format.readingDirection));
+    hb_buffer_set_language(hbBuffer, hb_language_from_string(layout.format.locale.c_str(), -1));
+
+    hb_shape(hbFont, hbBuffer, nullptr, 0);
+
+    uint32_t glyphCount = 0;
+    hb_glyph_info_t* glyphInfo = hb_buffer_get_glyph_infos(hbBuffer, &glyphCount);
+    hb_glyph_position_t* glyphPos = hb_buffer_get_glyph_positions(hbBuffer, &glyphCount);
+
+    float penX = 0.0f;
+    float penY = 0.0f;
 
     std::vector<GlyphPosition> glyphs;
+    glyphs.reserve(glyphCount);
 
-    hb_direction_t direction = toHBDirection(layout.format.readingDirection);
-    hb_font_t* hbFont = hb_ft_font_create(face, nullptr);
-
-    for (const auto& line : lines)
+    for (uint32_t i = 0; i < glyphCount; ++i)
     {
-        hb_buffer_t* hbBuffer = hb_buffer_create();
-        hb_buffer_add_utf8(hbBuffer, line.c_str(), -1, 0, -1);
+        uint32_t glyphIndex = glyphInfo[i].codepoint;
+        auto metrics = m_fontLoader->glyphMetrics(layout.format.font, static_cast<uint32_t>(layout.format.size), glyphIndex);
 
-        hb_buffer_set_direction(hbBuffer, direction);
-        hb_buffer_set_language(hbBuffer, hb_language_from_string(layout.format.locale.c_str(), -1));
+        Rectangle position{
+            penX + metrics.bearingX,
+            penY - metrics.bearingY,
+            metrics.width,
+            metrics.height
+        };
 
-        hb_shape(hbFont, hbBuffer, nullptr, 0);
+        glyphs.push_back(GlyphPosition{
+            .position = position,
+            .glyphIndex = glyphIndex
+        });
 
-        unsigned int glyphCount;
-        hb_glyph_info_t* glyphInfo = hb_buffer_get_glyph_infos(hbBuffer, &glyphCount);
-
-        for (unsigned int i = 0; i < glyphCount; i++)
-        {
-            FT_UInt glyphIndex = glyphInfo[i].codepoint;
-            GlyphPosition gPos = {
-                .position = Rectangle{}, // Position calculation can be added here
-                .glyphIndex = glyphIndex,
-            };
-            glyphs.push_back(gPos);
-        }
-
-        hb_buffer_destroy(hbBuffer);
+        penX += static_cast<float>(glyphPos[i].x_advance) / 64.0f;
+        penY += static_cast<float>(glyphPos[i].y_advance) / 64.0f;
     }
+
+    float minY = 0;
+    for (const auto& glyph : glyphs)
+    {
+        minY = std::min(minY, glyph.position.pos.y);
+    }
+    for (auto& glyph : glyphs)
+    {
+        glyph.position.pos.y -= minY;
+    }
+
+    hb_buffer_destroy(hbBuffer);
+    hb_font_destroy(hbFont);
 
     return glyphs;
 }
