253a01631e16113e517b124e67ad43c3cd3eb66a
kavos113 <kohei.hadano@gmail.com>
2026-03-11 15:28:32 +0900

    many rectangle layout

--- examples/gui/hello_layout/main.cpp
+++ examples/gui/hello_layout/main.cpp
@@ -1,6 +1,7 @@
 #include <karin/common.h>
 
 #include <memory>
+#include <random>
 
 #include "window.h"
 #include "container_node.h"
@@ -9,17 +10,22 @@
 
 int main()
 {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis(0.0f, 1.0f);
+
     karin::gui::Window window(L"Hello Layout", 100, 100, 800, 600);
 
     auto rootView = std::make_unique<karin::gui::ContainerNode>(karin::Size(800, 600));
 
-    auto redRect = std::make_unique<karin::gui::RectangleNode>(karin::Size(100, 100), karin::Color(karin::Color::Red));
-    auto greenRect = std::make_unique<karin::gui::RectangleNode>(karin::Size(100, 100), karin::Color(karin::Color::Green));
-    auto blueRect = std::make_unique<karin::gui::RectangleNode>(karin::Size(100, 100), karin::Color(karin::Color::Blue));
-
-    rootView->addChild(std::move(redRect));
-    rootView->addChild(std::move(greenRect));
-    rootView->addChild(std::move(blueRect));
+    for (int i = 0; i < 10; ++i)
+    {
+        auto rect = std::make_unique<karin::gui::RectangleNode>(
+            karin::Size(100, 100),
+            karin::Color(dis(gen), dis(gen), dis(gen))
+        );
+        rootView->addChild(std::move(rect));
+    }
 
     rootView->setLayoutDirection(karin::gui::ContainerNode::LayoutDirection::Row);
     rootView->setGap(10.0f);
