d72fb27b8c4eec6c5e2933b078ae28154c8d1697
kavos113 <kohei.hadano@gmail.com>
2026-03-14 18:13:11 +0900

    feat: text node

--- src/gui/text_node.h
+++ src/gui/text_node.h
@@ -1,23 +1,35 @@
 #ifndef SRC_GUI_TEXT_NODE_H
 #define SRC_GUI_TEXT_NODE_H
 
-#include <karin/graphics/text_layout.h>
+#include <karin/graphics/text_style.h>
+#include <karin/graphics/paragraph_style.h>
+#include <karin/graphics/pattern.h>
 
 #include "leaf_node.h"
+
+#include <string>
 
 namespace karin::gui
 {
 class TextNode : public LeafNode
 {
 public:
-    explicit TextNode(TextLayout layout);
+    explicit TextNode(
+        std::string text,
+        TextStyle textStyle,
+        ParagraphStyle paragraphStyle,
+        Pattern pattern
+    );
     ~TextNode() override = default;
 
     void drawInternal(GraphicsContext& gc, const Transform2D& parentTransform) const override;
     YGSize measure(Size availableSize) const override;
 
 private:
-    TextLayout m_layout;
+    std::string m_text;
+    TextStyle m_textStyle;
+    ParagraphStyle m_paragraphStyle;
+    Pattern m_pattern;
 };
 } // karin::gui
 
