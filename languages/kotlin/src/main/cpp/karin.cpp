382a73a1be88c19f53c13fd13b8367c16ad6d333
kavos113 <kohei.hadano@gmail.com>
2026-03-17 20:09:55 +0900

    feat: jni application cal

---
+++ languages/kotlin/src/main/cpp/karin.cpp
@@ -0,0 +1,31 @@
+#include "com_github_kavos113_karin_KarinJni.h"
+
+#include <karin/gui.h>
+
+using namespace karin::gui;
+
+JNIEXPORT jlong JNICALL Java_com_github_kavos113_karin_KarinJni_applicationCreate
+    (JNIEnv *env, jclass cls)
+{
+    auto *app = new Application();
+    return reinterpret_cast<jlong>(app);
+}
+
+JNIEXPORT jlong JNICALL Java_com_github_kavos113_karin_KarinJni_applicationCreateWindow
+    (JNIEnv *env, jclass cls, jlong appPtr, jstring title, jint x, jint y, jint width, jint height)
+{
+    auto *app = reinterpret_cast<Application *>(appPtr);
+
+    const char *titleChars = env->GetStringUTFChars(title, nullptr);
+    Window *window = app->createWindow(titleChars, x, y, width, height);
+    env->ReleaseStringUTFChars(title, titleChars);
+
+    return reinterpret_cast<jlong>(window);
+}
+
+JNIEXPORT void JNICALL Java_com_github_kavos113_karin_KarinJni_applicationRun
+    (JNIEnv *env, jclass cls, jlong appPtr)
+{
+    auto *app = reinterpret_cast<Application *>(appPtr);
+    app->run();
+}
