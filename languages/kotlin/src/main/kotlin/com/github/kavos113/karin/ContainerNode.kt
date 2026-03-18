fd278f8146c4237da495ace749c8be70eb8f0b2f
kavos113 <kohei.hadano@gmail.com>
2026-03-18 09:56:10 +0900

    feat: containernode enum value

--- languages/kotlin/src/main/kotlin/com/github/kavos113/karin/ContainerNode.kt
+++ languages/kotlin/src/main/kotlin/com/github/kavos113/karin/ContainerNode.kt
@@ -2,6 +2,17 @@ package com.github.kavos113.karin
 
 internal open class ContainerNode(ptr: Long) : ViewNode(ptr) {
 
+    enum class LayoutDirection(val value: Int) {
+        Row(0),
+        Column(1)
+    }
+
+    enum class WrapMode(val value: Int) {
+        NoWrap(0),
+        Wrap(1),
+        WrapReverse(2)
+    }
+
     constructor(): this(KarinJni.containerNodeCreate())
     constructor(size: Size) : this(KarinJni.containerNodeCreate(size.width, size.height))
 
@@ -9,5 +20,15 @@ internal open class ContainerNode(ptr: Long) : ViewNode(ptr) {
         KarinJni.containerNodeAddChild(nativePtr, child.nativePtr)
     }
 
+    fun setLayoutDirection(direction: LayoutDirection) {
+        KarinJni.containerNodeSetLayoutDirection(nativePtr, direction.value)
+    }
 
+    fun setWrapMode(wrapMode: WrapMode) {
+        KarinJni.containerNodeSetWrapMode(nativePtr, wrapMode.value)
+    }
+
+    fun setGap(gap: Float) {
+        KarinJni.containerNodeSetGap(nativePtr, gap)
+    }
 }
