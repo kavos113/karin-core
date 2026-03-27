4cb56fea12602f3c69a970db5c03913074ca9663
kavos113 <kohei.hadano@gmail.com>
2026-03-27 18:08:36 +0900

    feat: event dispatcher

---
+++ core/src/gui/event_dispatcher.cpp
@@ -0,0 +1,67 @@
+#include "event_dispatcher.h"
+
+#include <karin/common/geometry/point.h>
+
+namespace karin::gui
+{
+EventDispatcher::EventDispatcher(ViewNode* rootView)
+    : m_rootView(rootView)
+{
+}
+
+void EventDispatcher::dispatchEvent(const Event& event)
+{
+    if (!m_rootView)
+    {
+        return;
+    }
+
+    std::visit(
+        [this]<typename T0>(const T0& e)
+        {
+            using T = std::decay_t<T0>;
+            if constexpr (std::is_same_v<T, MouseMoveEvent>)
+            {
+                handleMouseMoveEvent(e);
+            }
+            else if constexpr (std::is_same_v<T, MouseButtonEvent>)
+            {
+                handleMouseButtonEvent(e);
+            }
+            else if constexpr (std::is_same_v<T, MouseWheelEvent>)
+            {
+                handleMouseWheelEvent(e);
+            }
+        },
+        event
+    );
+}
+
+void EventDispatcher::handleMouseMoveEvent(const MouseMoveEvent& event)
+{
+}
+
+void EventDispatcher::handleMouseButtonEvent(const MouseButtonEvent& event)
+{
+    ViewNode *target = m_rootView->hitTest(Point(static_cast<float>(event.x), static_cast<float>(event.y)));
+
+    switch (event.type)
+    {
+    case MouseButtonEvent::Type::ButtonPress_:
+        pressedNode = target;
+        break;
+
+    case MouseButtonEvent::Type::ButtonRelease_:
+        if (pressedNode && pressedNode == target)
+        {
+
+        }
+        pressedNode = nullptr;
+        break;
+    }
+}
+
+void EventDispatcher::handleMouseWheelEvent(const MouseWheelEvent& event)
+{
+}
+} // karin::gui
